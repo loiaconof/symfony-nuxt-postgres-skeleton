a609ca5d4034eb6169558f7a01eb26bb02bd996d
loiaconof <francescoloiacono95@outlook.it>
2024-04-28 11:32:08 +0200

    feat: update network name from app to webapp

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 const route = useRoute()
+const { data } = await useFetch('/test', { baseURL: 'http://localhost:8000'})
 </script>
 
 <template>
   <div>
     <h1>Nuxt prova!</h1>
+    data: {{ data }}
     <p>Current route: {{ route.path }}</p>
     <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
   </div>
