46794902da61ca7fb623dad536ee05a8712d4554
loiaconof <francescoloiacono95@outlook.it>
2024-05-19 18:16:18 +0200

    feat: update user table name

--- symfony/src/Entity/User.php
+++ symfony/src/Entity/User.php
@@ -8,7 +8,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`user`')]
+#[ORM\Table(name: '`l_user`')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_USERNAME', fields: ['username'])]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
