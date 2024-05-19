8e1a4ed01e7b1c837273cf1f91e251467c25b98a
loiaconof <francescoloiacono95@outlook.it>
2024-05-19 19:07:01 +0200

    feat: update security policy

--- symfony/src/Controller/TestController.php
+++ symfony/src/Controller/TestController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api')]
 class TestController extends AbstractController
 {
     #[Route('/test', name: 'app_test')]
