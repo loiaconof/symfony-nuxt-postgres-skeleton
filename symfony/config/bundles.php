ce356384a63d6a969955f7a14b82e64b4eb9f4f7
loiaconof <francescoloiacono95@outlook.it>
2024-05-19 18:48:03 +0200

    Revert "feat: composer require api-platform/core"
    This reverts commit 72ad703a861b5f8750cdcf9a516b39afb90baad8.

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -9,5 +9,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
-    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
